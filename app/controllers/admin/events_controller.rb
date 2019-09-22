cbb18618775d605ef280705d03d6ffa432afad83
xyy601chenyan <xyy545688506@gmail.com>
2019-09-23 07:55:55 +0800

    用户可以设定活动的状态

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -42,7 +42,7 @@ class Admin::EventsController < AdminController
   protected
 
   def event_params
-    params.require(:event).permit(:name, :description,:friendly_id)
+    params.require(:event).permit(:name, :description,:friendly_id, :status)
   end
 
   def find_event
