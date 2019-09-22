7658268b6e89d0bfa1fe6e03d5534a322e7a1014
xyy601chenyan <xyy545688506@gmail.com>
2019-09-23 07:29:53 +0800

    设置同一使用的默认的时间格式以及只显示年月日的时间格式

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,13 @@ module RailsRecipes
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #设置默认使用的语言
     config.i18n.default_locale = "zh-CN"
+
+    #设置默认的时区
     config.time_zone = "Beijing"
+
+    #设置默认的时间格式以及只显示年月日的时间格式ymd
+    Time::DATE_FORMATS.merge!(default: "%Y/%m/%d %I:%M %p", ymd: "%Y/%m/%d")
   end
 end
