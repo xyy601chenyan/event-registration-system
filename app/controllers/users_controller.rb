e24654bffdbb85696e173dc6d5f71d721d7d4016
xyy601chenyan <xyy545688506@gmail.com>
2019-09-24 11:06:13 +0800

    显示用户的资料

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_user
+
+  def show
+  end
 
   def edit
-    @user = current_user
-    @user.create_profile unless @user.profile
   end
 
   def update
-    @user = current_user
     if @user.update(user_params)
       flash[:notice] = "修改成功"
       redirect_to edit_user_path
@@ -20,5 +21,10 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:time_zone,:profile_attributes => [:id,
       :legal_name,:birthday,:location,:education,:occupation,:bio,:specialty])
+  end
+
+  def find_user
+    @user = current_user
+    @user.create_profile unless @user.profile
   end
 end
